77b13463a5f71c0ce29c50fb0e67006bd62f5f14
zhenzonglei <zhenzonglei@gmail.com>
2020-12-04 21:27:05 +0800

    ENH: automatic remove unresponse trial

--- responseEvaluation.m
+++ responseEvaluation.m
@@ -1,16 +1,18 @@
 function responseEvaluation(target,response,condName)
 % target, response,rt,condName
 
-[cVal,cMat,~,cPer] = confusion(target,response);
+idx = any(response,2);% only keep trial with response
+[cVal,cMat,~,cPer] = confusion(target(idx,:)',response(idx,:)');
 figure('Units','normalized','Position',[0 0 1 1])
 subplot(1,2,1), imagesc(cMat);
-title(sprintf('Accuracy = %.2f', 1 - cVal));
+title(sprintf('KeyProp = %.2f, Accuracy = %.2f',sum(idx)/length(target) ,1-cVal));
 axis square
 set(gca,'Xtick',1:length(cMat), 'XTickLabel',condName,...
     'Ytick',1:length(cMat),'YTickLabel', condName);
 colorbar
 subplot(1,2,2), bar(cPer);
 set(gca,'XTickLabel', condName);
+ylabel('Rate')
 axis square
 legend({'Miss','False alarm','Hit','Correct reject'},...
    'Orientation','vertical' ,'Location','northeastoutside' )
