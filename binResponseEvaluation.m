b99b27b2592effc72a90fc99f970216ecdbd4822
zhenzonglei <zhenzonglei@gmail.com>
2020-12-05 12:12:59 +0800

    NF: add text

--- binResponseEvaluation.m
+++ binResponseEvaluation.m
@@ -11,9 +11,10 @@ axis square
 set(gca,'Xtick',1:length(cMat), 'XTickLabel',condName,...
     'Ytick',1:length(cMat),'YTickLabel',condName);
 colorbar
-
-
-
+text(1,1,cPer(1,3),'FontSize',50,'Color','r');% hit
+text(1,2,cPer(1,1),'FontSize',50,'Color','r');% miss
+text(2,1,cPer(1,2),'FontSize',50,'Color','r');% false alarm
+text(2,2,cPer(1,4),'FontSize',50,'Color','r');% corect reject
 
 subplot(1,2,2), bar(cPer);
 set(gca,'XTickLabel',condName);
