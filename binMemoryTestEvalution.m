9a8e51d491000e932ae312e0acebe38b7b7f5004
zhenzonglei <zhenzonglei@gmail.com>
2020-12-05 16:49:21 +0800

    RF: merge 4 run figure into one

--- binMemoryTestEvalution.m
+++ binMemoryTestEvalution.m
@@ -1,12 +1,12 @@
 function binMemoryTestEvalution(subID,sessID)
 % function binMemoryTestEvalution(subID,sessID)
 % Evaluate memory performance from after fmri experiment
-
 workDir = pwd;
 % Create session dir
 sessDir = fullfile(workDir, 'data','fmri','train',...
     sprintf('sub%02d/sess%02d',subID,sessID));
-
+figure('Name',sprintf('sub%02d-sess%02d',subID,sessID),...
+    'Units','normalized','Position',[0 0 1 1])
 for runID = 1:4
     fileName =fullfile(sessDir,...
         sprintf('sub%02d_sess%02d_run%02d_beh.mat',subID,sessID,runID));
@@ -30,10 +30,20 @@ for runID = 1:4
     response(:,2) = trial(:,4) == -1; % unseen
     
     % Summarize the response with figure
-    binResponseEvaluation(target,response,{'Seen', 'Unseen'});
-    
-    % Save figure
-    figureFile = fullfile(sessDir,...
-        sprintf('sub%02d_sess%02d_run%02d_beh.jpg',subID,sessID,runID));
-    print(figureFile,'-djpeg');
+    idx = any(response,2);% only keep trial with response
+    [cVal,cMat,~,cPer] = binConfusion(target(idx,:)',response(idx,:)');
+    subplot(1,4,runID),
+    imagesc(cMat);
+    title(sprintf('RespProp = %.2f, Accuracy = %.2f',sum(idx)/length(target) ,1-cVal));
+    axis square; colorbar
+    condName = {'Seen', 'Unseen'};
+    set(gca,'Xtick',1:length(cMat), 'XTickLabel',condName,...
+        'Ytick',1:length(cMat),'YTickLabel',condName);
+    text(0.75,1,sprintf('%.2f',cPer(1,3)),'FontSize',50,'Color','r');% hit
+    text(0.75,2,sprintf('%.2f',cPer(1,1)),'FontSize',50,'Color','r');% miss
+    text(1.75,1,sprintf('%.2f',cPer(1,2)),'FontSize',50,'Color','r');% false alarm
+    text(1.75,2,sprintf('%.2f',cPer(1,4)),'FontSize',50,'Color','r');% corect reject
 end
+% Save figure
+figureFile = fullfile(sessDir,sprintf('sub%02d_sess%02d_beh.jpg',subID,sessID));
+print(figureFile,'-djpeg');
