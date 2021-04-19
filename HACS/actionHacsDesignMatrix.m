e9c8cd6c8cd56c0fc8c1576668c8a6574c94bfeb
Blink <1458940886@qq.com>
2021-04-19 09:32:11 -0400

    ENH: action design matrix

--- HACS/actionHacsDesignMatrix.m
+++ HACS/actionHacsDesignMatrix.m
@@ -18,15 +18,15 @@ className = C{2}; % action class name, 200x1, cell array
 superClassID = C{3}; % 14 superClass ID, 200x1, int array, 
 superClassName = C{4}; % super class name, 200x1 cell array
 
-nClass = 200; 
-nSuperClass = 14;
-nSession = 60;
+nClass = 180;
+nSuperClass = 13;
+nSession = 120;
 nRepeat = 4; % repeat times of all class in one session
 nSessionTmp = nSession*nRepeat; 
 nStimPerSession = nClass*nRepeat;
 
 %% Organize stimulus according to the super class info 
-stimulus = cell(nClass,nSessionTmp); % 200x240 cell array
+stimulus = cell(nClass,nSessionTmp); % 180x480 cell array
 for i = 1:length(className) % class loop
     videoName = dir(fullfile(videoDir,className{i})); 
     videoName = extractfield(videoName(3:end), 'name');
@@ -36,7 +36,7 @@ end
 
 %% Load optseq of super class 
 optSeqSuperClass = NaN(nClass,nSessionTmp,3);% [onset, class, dur]
-for s = 1:nRepeat % session loop
+for s = 1:nSessionTmp % session loop
     % Read par from optseq
     optSeqSuperClassFile = fullfile(designDir,'sessPar',...
         sprintf('action-session-%03d.par',s));
@@ -53,7 +53,7 @@ end
 
 %% Translate superClass optSeq to class optSeq
 optSeqClass = optSeqSuperClass;
-for s = 1:nRepeat % session loop
+for s = 1:nSessionTmp % session loop
     for c = 1:nSuperClass % class loop
         superClassTrial = (optSeqSuperClass(:,s,2) == c);
         classTrial = find(superClassID == c);       
@@ -63,15 +63,15 @@ end
 % check output: eg=squeeze(optSeqClass(:,1,:));
 
 %% Reshape stimulus and design sequence as required
-% reshape stimulus to 800x60 array
-% reshape optSeqSuperClass&optSeqClass to 800x60x3 array
+% reshape stimulus to 720x120 array
+% reshape optSeqSuperClass&optSeqClass to 720x120x3 array
 stimulus = reshape(stimulus,nStimPerSession,nSession); 
 optSeqClass = reshape(optSeqClass,nStimPerSession,nSession,3); 
 optSeqSuperClass = reshape(optSeqSuperClass,...
         nStimPerSession,nSession,3); 
 
 %% Replace optSeq timing with Kay design
-nRun = 8; runDur = 476; trialDur = 4; % in seconds
+nRun = 12; runDur = 284; trialDur = 4; % in seconds
 kaySeq = 0:trialDur:runDur; % Trials are uniformly-spaced in time(i.e., 4s)
 kaySeq(6:6:length(kaySeq)) = []; % Remove null trials(evey sixth trials)
 onset = repmat(kaySeq, [1,nRun]);
@@ -82,12 +82,12 @@ end
  
 %% Pack and save action strcture
 action.desp = 'actionHacs session-level paradigm';
-action.className = className; % HACS class name, 200x1, cell array
-action.superClassName = superClassName;% HACS superclass id, 200x1, cell array
-action.superClassID = superClassID;% HACS superclass class id, 200x1, int array
-action.stimulus = stimulus; % 800 x 60, cell array
-action.paradigmSuperClass = optSeqSuperClass; % 800(nClass*4) x 60(session) x 3 array
-action.paradigmClass = optSeqClass; % 800(nClass*4) x 60(session) x 3  array
+action.className = className; % HACS class name, 180x1, cell array
+action.superClassName = superClassName;% HACS superclass id, 180x1, cell array
+action.superClassID = superClassID;% HACS superclass class id, 180x1, int array
+action.stimulus = stimulus; % 720 x 120, cell array
+action.paradigmSuperClass = optSeqSuperClass; % 720(nClass*4) x 120(session) x 3 array
+action.paradigmClass = optSeqClass; % 720(nClass*4) x 120(session) x 3  array
 action.date = datetime; 
 
 % Save action to design dir
